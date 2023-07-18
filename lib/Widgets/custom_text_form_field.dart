3de63a01eea679f9709d7754524cfb9998705170
dddddeeeevvvaaor <prizandevaokturarizqy@gmail.com>
2023-07-18 18:50:09 +0700

    add CustomTextFormFieldEmail

--- lib/Widgets/custom_text_form_field.dart
+++ lib/Widgets/custom_text_form_field.dart
@@ -40,3 +40,44 @@ class CustomTextFormField extends StatelessWidget {
     );
   }
 }
+
+
+class CustomTextFormFieldEmail extends StatelessWidget {
+  const CustomTextFormFieldEmail({
+    Key? key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+    required this.icon,
+    required Null Function(dynamic value) onSaved,
+  }) : super(key: key);
+  final TextEditingController controller;
+  final String hintText;
+  final bool obscureText;
+  final IconData icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, right: 20, bottom: 10),
+      child: TextFormField(
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: Theme.of(context).textTheme.bodyText1,
+          prefixIcon: const Icon(Icons.email, color: Colors.blue),
+          suffixIcon:
+              const ColouredPokeball(color: Colors.blue, width: 20, opacity: 1),
+        ),
+        validator: (value) {
+          if (value == null || value.isEmpty) {
+            return 'Please enter your email';
+          } else if (!value.contains('@')) {
+            return 'Please enter a valid email address';
+          }
+          return null;
+        },
+      ),
+    );
+  }
+}
